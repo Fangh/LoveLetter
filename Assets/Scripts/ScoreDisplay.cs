f538d5715ea71f1a739474afaa6ef31298df2f43
Esefen <romain.soual.pro@gmail.com>
2017-01-21 18:27:53 +0100

    ScoreDisplay.cs

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (scoreDisplay != null)
+        {
+            scoreDisplay.text = GameManager.Instance.score + " / " + GameManager.Instance.scoreThreshold;
+        }
+	}
+}
