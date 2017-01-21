887b85614f696883f430e6e74b19c6ea690d90c4
Unknown <toto.klein@laposte.net>
2017-01-21 12:53:51 +0100

    StartGame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,40 @@ using System.Collections;
 public class GameManager : MonoBehaviour 
 {
 	public static GameManager Instance;
+	public GameObject P1Playground;
+	public GameObject P2Playground;
+	public bool forceStartGame = false;
+	bool GameIsStarted = false;
 	GameObject ball;
 
 	// Use this for initialization
 	void Start () 
 	{
-		ball = GameObject.FindWithTag ("Ball");
-	
+		ball = GameObject.FindWithTag ("Ball");	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if (!GameIsStarted) 
+		{
+			if (P1Playground.GetComponent<Playground> ().validGameState && P2Playground.GetComponent<Playground> ().validGameState) 
+			{
+				StartGame ();
+			}
+			if (forceStartGame) 
+			{
+				forceStartGame = false;
+				StartGame ();				
+			}
+		}
 	
+	}
+
+	void StartGame()
+	{
+		GameIsStarted = true;
+		RespawnBall ();		
 	}
 
 	public void RespawnBall()
