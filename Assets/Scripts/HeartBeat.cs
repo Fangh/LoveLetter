e7fa37591803216cf51639660121c8f09c741381
Fangh <thomas.klein@astrakhan.fr>
2019-01-19 10:58:41 +0100

    I don't remember what this is

---
+++ Assets/Scripts/HeartBeat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HeartBeat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.DOScale (new Vector3(20, 20, 20), 0.25f).SetLoops(-1,LoopType.Yoyo);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
